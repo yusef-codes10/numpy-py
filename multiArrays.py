a392761e41621922da2f0592719e24a682bee6e2
Youcef <youcefcopy@gmail.com>
2025-12-21 19:30:00 +0100

    @D array aka matrix

--- multiArrays.py
+++ multiArrays.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# this is a 0 dimensional array (single value)
+# 1- this is a 0 dimensional array (single value)
 array = np.array('!')
 
 # number of dimension using the ndim property
@@ -13,3 +13,11 @@ vector = np.array([1, 2, 3, 4])
 
 print(f"Dimenions: {vector.ndim}")
 print(vector)
+
+# 3- this a 2D array (Matrix)
+matrix = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+print(f"Dimenions: {matrix.ndim}")
+print(matrix)
