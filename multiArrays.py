37718114e2d2efaf85a26e73700fa94d9c36edb5
Youcef <youcefcopy@gmail.com>
2025-12-21 19:25:51 +0100

    1 dimensional array (vector, 1 row)

--- multiArrays.py
+++ multiArrays.py
@@ -5,5 +5,11 @@ array = np.array('!')
 
 # number of dimension using the ndim property
 
-print(array.ndim)
+print(f"Dimenions: {array.ndim}")
 print(array)
+
+# 2- this is 1 dimensional array (1 row)
+vector = np.array([1, 2, 3, 4])
+
+print(f"Dimenions: {vector.ndim}")
+print(vector)
