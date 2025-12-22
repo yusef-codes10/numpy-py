8aff73eb13742bfb4c2f18e266d9399f6d370d36
Youcef <youcefcopy@gmail.com>
2025-12-22 21:06:26 +0100

    the issue was not importing np, and alphabet was a regular tuple

--- alphabet.py
+++ alphabet.py
@@ -1,8 +1,10 @@
-alphabet = [
+import numpy as np
+
+alphabet = np.array([
     [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']],
     [['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r']],
     [['s', 't', 'u'], ['v', 'w', 'x'], ['y', 'z', ' ']]   
-]
+])
 
 print(alphabet)
 
