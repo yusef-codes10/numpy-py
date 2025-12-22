e918a3df6131d6beb1fbcba66cf0565fba018ade
Youcef <youcefcopy@gmail.com>
2025-12-22 12:17:27 +0100

    this is how we access different arrays & differenet values

--- multiArrays.py
+++ multiArrays.py
@@ -3,24 +3,14 @@ import numpy as np
 # 1- this is a 0 dimensional array (single value)
 array = np.array('!')
 
-# number of dimension using the ndim property
-
-print(f"Dimenions: {array.ndim}")
-print(array)
-
 # 2- this is 1 dimensional array (1 row)
 vector = np.array([1, 2, 3, 4])
-
-print(f"Dimenions: {vector.ndim}")
-print(vector)
 
 # 3- this a 2D array (Matrix)
 matrix = np.array([[1, 2, 3],
                   [4, 5, 6],
                   [7, 8, 9]])
 
-print(f"Dimenions: {matrix.ndim}")
-print(matrix)
 
 # 4- 3D array (beyonf imagination)
 matrix3D = np.array(
@@ -31,6 +21,15 @@ matrix3D = np.array(
 
 print(f"Dimenions: {matrix3D.ndim}")
 print(matrix3D)
+
+# accessing the 1D array value
+print(vector[0])
+
+# accessing the 2D array value
+print(matrix[0][0])
+
+# accessing the 3D array value
+print(matrix3D[0][0][0])
 
 
 # ! key lesson: a n-D array is basically an array of (n-1D) arrays
