439703ef0d7e1aada70c5d6e70a1db95d355f75a
Youcef <youcefcopy@gmail.com>
2025-12-24 19:59:48 +0100

    we will be writing a clice expression

--- slicing.py
+++ slicing.py
@@ -1,4 +1,9 @@
 import numpy as np
 
 # we need a 2D array to work with
-array = np.array([[], [], [], []])
+array = np.array([[1, 2, 3, 4],
+                  [5, 6, 7, 8],
+                  [9, 10, 11, 12],
+                  [13, 14, 15, 16]])
+
+# array['slice expression']
